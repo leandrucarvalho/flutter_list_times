75f950307b862045665381377b38801f2ae3b802
leandrucarvalho <leandrucs@gmail.com>
2023-05-16 18:55:34 -0300

    configuring firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_list_times/controller/home_controller.dart';
 import 'package:flutter_list_times/controller/theme_controller.dart';
+import 'package:flutter_list_times/firebase_options.dart';
 import 'package:flutter_list_times/pages/home_page.dart';
 import 'package:flutter_list_times/repository/times_repository.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MyApp(),
   );
